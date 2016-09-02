33ed3091beb3c0c19a738571474323dcc8ffe59a
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-09-02 13:45:21 +0200

    wip

--- src/MapGenerator.hx
+++ src/MapGenerator.hx
@@ -4,6 +4,7 @@ import ash.systems.*;
 import ash.fsm.*;
 import components.*;
 import gengine.math.*;
+import haxe.ds.Vector;
 
 class MapGenerator
 {
@@ -16,8 +17,19 @@ class MapGenerator
 
     static public function generate()
     {
+        var size = 128;
         var background = Factory.createBackground(new Vector2(2048, 2048));
-
         engine.addEntity(background);
+
+        var data = new Vector<Int>(size);
+        var lakesCount = 3;
+
+        while(lakesCount > 0)
+        {
+            var x = Std.random(size);
+            var y = Std.random(size);
+
+            lakesCount--;
+        }
     }
 }
