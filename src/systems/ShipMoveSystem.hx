46e8ce23eeee72892af30fc8e9bd5296d8c746a6
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-10-04 13:45:04 +0200

    Wip on movements

--- src/systems/ShipMoveSystem.hx
+++ src/systems/ShipMoveSystem.hx
@@ -20,11 +20,11 @@ class ShipMoveSystem extends ListIteratingSystem<ShipMoveNode>
             points.push(Factory.createPoint());
         }
     }
-    
+
     public override function addToEngine(engine:Engine)
     {
         super.addToEngine(engine);
-        
+
         for(i in 0...100)
         {
             engine.addEntity(points[i]);
@@ -41,33 +41,38 @@ class ShipMoveSystem extends ListIteratingSystem<ShipMoveNode>
         {
             var currentPos = node.entity.position;
             var nextPos:Vector3 = null;
-            
+
             for(i in 0...currentPath.length)
             {
-                nextPos = currentPath[i];
-                break;
+                if(Maths.getVector3DistanceSquared(currentPos, currentPath[i]) > 16 * 16)
+                {
+                    nextPos = currentPath[i];
+                }
             }
 
             if(nextPos != null)
             {
                 var velo = new Vector2(nextPos.x - currentPos.x, nextPos.y - currentPos.y);
 
+                velo = Maths.getNormalizedVector2(velo) * 100.0;
+
                 node.body.setLinearVelocity(velo);
-                
+
                 for(i in 0...currentPath.length)
                 {
                     if(i>=100) break;
                     points[i].position = currentPath[i];
                 }
             }
+            else
+            {
+                node.body.setLinearVelocity(new Vector2(0, 0));
+            }
         }
     }
 
     private function onNodeAdded(node:ShipMoveNode)
     {
-
-        trace("target",node.ship.targetPosition.x, node.ship.targetPosition.y);
-
     }
 
     private function onNodeRemoved(node:ShipMoveNode)
